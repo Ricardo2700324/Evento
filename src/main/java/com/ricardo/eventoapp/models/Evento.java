3e9dc9e33408a9cadc8cff9180050f8477847edd
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2019-09-18 10:55:39 -0300

    Conectando com mysql

--- src/main/java/com/ricardo/eventoapp/models/Evento.java
+++ src/main/java/com/ricardo/eventoapp/models/Evento.java
@@ -1,7 +1,20 @@
 package com.ricardo.eventoapp.models;
 
-public class Evento {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Evento implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
 	
 	private String nome;
 	private String local;
@@ -11,6 +24,12 @@ public class Evento {
 	
 	
 	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
 	public String getNome() {
 		return nome;
 	}
