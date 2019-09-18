30f7e38b977ae763e12c1d734c8acb2dcf3eb808
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2019-09-18 10:20:48 -0300

    Cadastro de Evento

--- src/main/java/com/ricardo/eventoapp/controllers/IndexController.java
+++ src/main/java/com/ricardo/eventoapp/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package com.ricardo.eventoapp;
+package com.ricardo.eventoapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +9,6 @@ public class IndexController {
 	@RequestMapping("/")
 	public String index() {
 		return "index";
-	} 
-	
+	}
+
 }
