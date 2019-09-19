6417a5fb1878bf3d59fde2ccc4d13d05515ed900
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2019-09-19 13:59:25 -0300

    Realizando buscas específicas no banco de dados

--- src/main/java/com/ricardo/eventoapp/repository/ConvidadoRepository.java
+++ src/main/java/com/ricardo/eventoapp/repository/ConvidadoRepository.java
@@ -3,7 +3,9 @@ package com.ricardo.eventoapp.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.ricardo.eventoapp.models.Convidado;
+import com.ricardo.eventoapp.models.Evento;
 
 public interface ConvidadoRepository extends CrudRepository<Convidado, String> {
 
+	Iterable<Convidado> findByEvento(Evento evento);
 }
